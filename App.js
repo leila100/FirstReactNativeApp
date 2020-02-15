b8152081076780ac79545da6ea3a5fd0ed4f4851
Leila <nb.leila10@gmail.com>
2020-02-15 17:49:52 -0600

    Add input field to a modal
    Add cancel button
    Close modal when adding a goal

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from "react-native";
+import { StyleSheet, View, Button, FlatList } from "react-native";
 
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
   const [goals, setGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalHandler = goal => {
     setGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: goal }]);
+    setIsAddMode(false);
   };
 
   const deleteHandler = id => {
     setGoals(currentGoals => currentGoals.filter(goal => goal.id !== id));
   };
 
+  const cancelHandler = () => {
+    setIsAddMode(false);
+  };
+
   return (
     <View style={styles.screen}>
-      <GoalInput addGoalHandler={addGoalHandler} />
+      <Button title='Add New Goal' onPress={() => setIsAddMode(true)} />
+      <GoalInput addGoalHandler={addGoalHandler} onCancel={cancelHandler} visible={isAddMode} />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={goals}
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, TextInput, Button, StyleSheet } from "react-native";
+import { View, TextInput, Button, StyleSheet, Modal } from "react-native";
 
 const GoalInput = props => {
   const [goal, setGoal] = useState("");
@@ -8,16 +8,27 @@ const GoalInput = props => {
     setGoal(text);
   };
 
+  const onAdd = () => {
+    props.addGoalHandler(goal);
+    setGoal("");
+  };
+
   return (
-    <View style={styles.entry}>
-      <TextInput placeholder='Course Goal' style={styles.textInput} onChangeText={inputHandler} value={goal} />
-      <Button title='ADD' onPress={() => props.addGoalHandler(goal)} />
-    </View>
+    <Modal visible={props.visible} animationType='slide'>
+      <View style={styles.entry}>
+        <TextInput placeholder='Course Goal' style={styles.textInput} onChangeText={inputHandler} value={goal} />
+        <View style={styles.buttons}>
+          <Button title='ADD' onPress={onAdd} />
+          <Button title='CANCEL' onPress={props.onCancel} color='red' />
+        </View>
+      </View>
+    </Modal>
   );
 };
 const styles = StyleSheet.create({
-  entry: { flexDirection: "row", justifyContent: "space-between", alignItems: "center" },
-  textInput: { borderColor: "black", borderWidth: 1, padding: 10, width: "80%" }
+  entry: { flex: 1, justifyContent: "center", alignItems: "center" },
+  textInput: { borderColor: "black", borderWidth: 1, padding: 10, width: "80%", marginBottom: 10 },
+  buttons: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "40%" }
 });
 
 export default GoalInput;
